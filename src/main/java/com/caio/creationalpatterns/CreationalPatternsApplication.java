c49c6cae831829bf4fc7d08a98938fe688052703
Caio-Santana <caio.santana@live.com>
2022-10-15 15:19:07 -0300

    Add design pattern Singleton

---
+++ src/main/java/com/caio/creationalpatterns/CreationalPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.caio.creationalpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreationalPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreationalPatternsApplication.class, args);
+	}
+
+}
